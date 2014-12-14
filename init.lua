3b4d3b85c3a1e14037ba9a5f59bc5c628c17c055
SmallJoker <mk939@ymail.com>
2014-12-14 13:00:49 +0100

    Make sign repair tool visible in the creative inventory

--- init.lua
+++ init.lua
@@ -19,9 +19,6 @@ end
 local modpath = minetest.get_modpath("special")
 dofile(modpath.."/chatcommands.lua")
 dofile(modpath.."/craftings.lua")
-if special_is_enabled("sign_repair") then
-	dofile(modpath.."/signrepair.lua")
-end
 if special_is_enabled("cleaner_tool") then
 	dofile(modpath.."/cleaner.lua")
 end
@@ -78,5 +75,25 @@ minetest.register_on_leaveplayer(function(player)
 	end
 end)
 -- ] SET HEALTH END
+
+if special_is_enabled("sign_repair") then
+	minetest.register_tool("special:signrepair", {
+		description = "Sign repair tool",
+		inventory_image = "anytool.png",
+		stack_max = 1,
+		on_use = function(itemstack, player, pointed_thing)
+			if pointed_thing.type ~= "node" then
+				return
+			end
+			local meta = minetest.get_meta(pointed_thing.under)
+			if meta and meta:get_string("formspec") == "hack:sign_text_input" then
+				meta:set_string("formspec", "field[text;;${text}]")
+				minetest.chat_send_player(player:get_player_name(), "Repaired "..minetest.pos_to_string(pointed_thing.under))
+			end
+			itemstack:add_wear(170)
+			return itemstack
+		end
+	})
+end
 
 dofile(modpath.."/craftings.lua")
